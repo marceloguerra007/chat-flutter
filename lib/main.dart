ba79e0b3a6b6d100259859ad3de541dc9ab01de2
marcelo GB <marceloguerra007@gmail.com>
2021-10-27 20:39:20 -0300

    Estrutura inicial do projeto.

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:chat/pages/loading_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoadingPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
