ab450fdc5f5b0af961840809acfd4ccc07206737
marcelo GB <marceloguerra007@gmail.com>
2021-11-15 12:03:48 -0300

    Estrutura inicial da tela de Chat Page.

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,21 +1,53 @@
+import 'package:chat/components/messages.dart';
+import 'package:chat/components/new_messages.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({ Key? key }) : super(key: key);
 
+  static const int LOGOUT = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Chat Page'),
-          TextButton(
-            child: Text('Logout'),
-            onPressed: ()=> AuthService().logout()
+      appBar: AppBar(
+        title: Text('Chat de Amigos'),
+        actions: [
+          DropdownButton(
+            icon: Icon(Icons.more_vert, color: Theme.of(context).primaryIconTheme.color),
+            items: [
+              DropdownMenuItem(
+                value: LOGOUT,
+                child: Container(
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Colors.black87,
+                      ),
+                      SizedBox(width: 10),
+                      Text('Sair')
+                    ],
+                  ),
+                )
+              )
+            ],
+            onChanged: (value) {
+              if (value == LOGOUT){
+                AuthService().logout();
+              }
+            },            
           )
         ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(child: Messages()),
+            NewMessages()
+          ],
+        ),
       ),
     );
   }
