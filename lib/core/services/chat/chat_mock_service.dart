e70c85f083d5c9636213174138b4972105c0dfbf
marcelo GB <marceloguerra007@gmail.com>
2021-11-17 20:33:13 -0300

    Estrutura inicial do serviço referente ao chat.

---
+++ lib/core/services/chat/chat_mock_service.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/models/chat_message.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
+
+class ChatMockService implements ChatService{
+  static final List<ChatMessage> _msgs = [
+    ChatMessage(
+      id: '1', 
+      text: 'Olá, bem vindo', 
+      createdAt: DateTime.now(), 
+      userId: '11', 
+      userName: 'Klara', 
+      userImageURL: 'assets/images/avatar.png')
+  ];
+  static MultiStreamController<List<ChatMessage>>? _controller;
+  static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
+    _controller = controller;
+  });
+  
+  @override
+  Stream<List<ChatMessage>> messagesStream() {
+    return _msgsStream;
+  }
+
+  @override
+  Future<ChatMessage> save(String text, ChatUser user) async {
+    final newMessage = ChatMessage(
+      id: Random().nextDouble().toString(), 
+      text: text, 
+      createdAt: DateTime.now(), 
+      userId: user.id, 
+      userName: user.name, 
+      userImageURL: user.imageURL
+    );
+    
+    _msgs.add(newMessage);      
+    _controller?.add(_msgs);
+
+    return newMessage;
+  }
+
+}
