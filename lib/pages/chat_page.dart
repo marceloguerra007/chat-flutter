099e4f8a2fecb4d153c367093bacf1efbc153696
marcelo GB <marceloguerra007@gmail.com>
2021-11-20 17:03:11 -0300

    Implementação inicial do serviço de notificação.

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,10 @@
 import 'package:chat/components/messages.dart';
 import 'package:chat/components/new_messages.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/core/services/notification/chat_notification_service.dart';
+import 'package:chat/pages/notification_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({ Key? key }) : super(key: key);
@@ -14,30 +17,60 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Chat de Amigos'),
         actions: [
-          DropdownButton(
-            icon: Icon(Icons.more_vert, color: Theme.of(context).primaryIconTheme.color),
-            items: [
-              DropdownMenuItem(
-                value: LOGOUT,
-                child: Container(
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.exit_to_app,
-                        color: Colors.black87,
-                      ),
-                      SizedBox(width: 10),
-                      Text('Sair')
-                    ],
-                  ),
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
+              icon: Icon(Icons.more_vert, color: Theme.of(context).primaryIconTheme.color),
+              items: [
+                DropdownMenuItem(
+                  value: LOGOUT,
+                  child: Container(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.exit_to_app,
+                          color: Colors.black87,
+                        ),
+                        SizedBox(width: 10),
+                        Text('Sair')
+                      ],
+                    ),
+                  )
                 )
+              ],
+              onChanged: (value) {
+                if (value == LOGOUT){
+                  AuthService().logout();
+                }
+              },            
+            ),
+          ),
+          Stack(
+            children: [
+              IconButton(
+                icon: Icon(Icons.notifications),
+                onPressed: (){
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (ctx) {
+                        return NotificationPage();
+                      }
+                    )
+                  );
+                },
+              ),
+              Positioned(
+                top: 5,
+                right: 5, 
+                child: CircleAvatar(
+                  maxRadius: 10,
+                  backgroundColor: Colors.red.shade800,
+                  child: Text(
+                    '${Provider.of<ChatNotificationService>(context).itemsCount}',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
               )
             ],
-            onChanged: (value) {
-              if (value == LOGOUT){
-                AuthService().logout();
-              }
-            },            
           )
         ],
       ),
---
+++ lib/pages/notification_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notificações'),
+      ),
+      body: Center(child: Text('Suas notificações')),
+    );
+  }
+}
