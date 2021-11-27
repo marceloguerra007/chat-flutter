979331bcf63fe95fcfef1aa2afc8c8234140e5bd
marcelo GB <marceloguerra007@gmail.com>
2021-11-27 11:23:50 -0300

    Complemento da autenticação com Firebase

--- lib/core/services/auth/auth_firebase_service.dart
+++ lib/core/services/auth/auth_firebase_service.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class AuthFirebaseService implements AuthService{
   static ChatUser? _currentUser;
@@ -48,16 +50,45 @@ class AuthFirebaseService implements AuthService{
     if (credential == null)
       return;
 
-    credential.user?.updateDisplayName(name);
-    //credential.user?.updatePhotoURL()
+    final imageName = '${credential.user!.uid}.jpg';
+    final imageURL = await _uploadUserImage(image, imageName);
+
+    await credential.user?.updateDisplayName(name);
+    await credential.user?.updatePhotoURL(imageURL);
+
+    await _saveChatUser(_toChatUser(credential.user!, imageURL));
   }
 
-  static ChatUser _toChatUser(User user){
+  static ChatUser _toChatUser(User user, [String? imageURL]){
     return ChatUser(
       id: user.uid, 
       name: user.displayName ?? user.email!.split('@')[0], 
       email: user.email!, 
-      imageURL: user.photoURL ?? 'assets/images/avatar.png'
+      imageURL: imageURL ?? user.photoURL ?? 'assets/images/avatar.png'
     );
   }
+
+  Future<String?> _uploadUserImage(File? image, String imageName) async{
+    if (image == null)
+      return null;
+    
+    final storage = FirebaseStorage.instance;
+    final imageRef = storage.ref().child('user_images').child(imageName);
+
+    await imageRef.putFile(image).whenComplete(() => null);
+
+    return await imageRef.getDownloadURL();
+  }
+
+  Future<void> _saveChatUser(ChatUser user) async{
+    final store = FirebaseFirestore.instance;
+    final docRef = store.collection('users').doc(user.id);
+
+    return docRef.set({
+      'name': user.name,
+      'email': user.email,
+      'imageURL': user.imageURL,
+      'createdAt': DateTime.now().toIso8601String()
+    });
+  } 
 }
